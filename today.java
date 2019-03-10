d52245c9dd49dc4aee6ebf7007909afdd314d7f5
Niranjan Singh M <niranjansingh92@gmail.com>
2019-03-10 11:40:00 +0000

    updated in the dev branch

--- today.java
+++ today.java
@@ -1,2 +1,4 @@
 today is sunday 10th of march 2019. 
-today we cannot go to the mall in the evening.
+today we cannot go to the mall in the evening
+
+where is the meenakshi mall located in Bengaluru.
