5b11eb21746e4ff85c250339e8be7479e21224f3
Niranjan Singh M <niranjansingh92@gmail.com>
2019-03-10 10:49:35 +0000

    Updated in the developement branch

---
+++ sunday.java
@@ -0,0 +1,2 @@
+todya is 10th of march sunday. we started the day with online classes.
+in the evening also we have classes.
---
+++ today.java
@@ -0,0 +1,2 @@
+today is sunday 10th of march 2019. 
+today we cannot go to the mall in the evening.
